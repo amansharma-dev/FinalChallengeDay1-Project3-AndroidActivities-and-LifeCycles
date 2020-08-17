a34b5f4419ffac0a66102f8dd81b31250c0e028e
AmanSharma <amansharma.11@hotmail.com>
2020-08-17 16:16:03 +0530

    commit

--- app/src/main/java/com/example/finalchallengeday1_project3_androidactivities_and_lifecycles/MainActivity.java
+++ app/src/main/java/com/example/finalchallengeday1_project3_androidactivities_and_lifecycles/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.finalchallengeday1_project3_androidactivities_and_lifecycles
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+
+
 }
